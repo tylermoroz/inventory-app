ca762c75f9b7df829957bf37ca1e96760e38d31c
Tyler Moroz <moroztyler@gmail.com>
2026-09-07 14:57:48 -0600

    Update pool connection logic.

--- db/pool.js
+++ db/pool.js
@@ -1,5 +1,8 @@
 const { Pool } = require("pg");
 
 module.exports = new Pool({
-  connectionString: process.env.LOCAL_URL,
+  connectionString:
+    process.env.NODE_ENV === "production"
+      ? process.env.DATABASE_URL
+      : process.env.LOCAL_URL,
 });
